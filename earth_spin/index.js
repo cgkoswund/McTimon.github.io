c26551a46700e93c560b6aed6ad055574b318c30
periwinkle <mctimon3d@gmail.com>
2021-01-28 15:27:14 +0000

    adding clickable divs

--- earth_spin/index.js
+++ earth_spin/index.js
@@ -4,27 +4,46 @@
         import { RoughnessMipmapper } from './RoughnessMipmapper.js';
         import Bowser from './browser_detect_library/bowser.js';
 
+        //THREE.js global variables
         let camera, scene, renderer, mixer, aspect;
         let clock = new THREE.Clock();
-        let model, mainScene, frames;
+        let model;
 
+        //variables for tracking scroll and syncing timeline
         let scrollBuffer = 0; 
         let currentScroll = 0;
         let scrollDelta = 0; 
         let el;
-        let elapsedTime = 0;
 
+        //hidden divs, variables for click handling of buttons
+        let firstHiddenDiv, secondHiddenDiv, thirdHiddenDiv;
+        let firstHiddenButton, secondHiddenButton, thirdHiddenButton;
+
+        //variables for restricting animation to frame range,
+        //and for unhiding divs at appropriate frames
+        let currentAnimationFrame, currentTime = 0;
+
+        //brower detect for balancing scroll speed
         const browserName = Bowser.parse(window.navigator.userAgent).browser.name;
+
+        //overall scroll speed factor
+        const scrollSpeed = 10;
 
         init();
         render();
 
         function init() {
 
-            const container = document.querySelector( 'div' );
+            const container = document.querySelector( '#hidden-button-bg' );
             const canvas = document.querySelector("#c");
-            // const container = document.createElement( 'div' );
-            document.body.appendChild( container );
+
+            firstHiddenDiv = document.querySelector("#first");
+            secondHiddenDiv = document.querySelector("#second");
+            thirdHiddenDiv = document.querySelector("#third");
+
+            firstHiddenButton = document.querySelector("#first-button");
+            secondHiddenButton = document.querySelector("#second-button");
+            thirdHiddenButton = document.querySelector("#third-button");
             el = container;
 
             camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.00025, 20 );
@@ -32,6 +51,7 @@
 
             scene = new THREE.Scene();
 
+            //load evironment texture (for good lighting)
             new RGBELoader()
                 .setDataType( THREE.UnsignedByteType )
                 .setPath( 'textures/equirectangular/' )
@@ -47,14 +67,12 @@
 
                     const roughnessMipmapper = new RoughnessMipmapper( renderer );
 
+                    //load 3D model (and animation)
                     const loader = new GLTFLoader().setPath( 'earth_model/' );
                     loader.load( 'scene.gltf', function ( gltf ) {
 
                         model = gltf.scene;
-                        mainScene = gltf;
-                        frames = mainScene.animations[0].tracks[0].times;
                         scene.add(model);
-                        console.log(mainScene);
 
                         mixer = new THREE.AnimationMixer(model);
                         mixer.clipAction(gltf.animations[0]).play();
@@ -74,7 +92,6 @@
             renderer.toneMapping = THREE.ACESFilmicToneMapping;
             renderer.toneMappingExposure = 1;
             renderer.outputEncoding = THREE.sRGBEncoding;
-            // container.appendChild( renderer.domElement );
 
 
             const pmremGenerator = new THREE.PMREMGenerator( renderer );
@@ -84,52 +101,95 @@
 
         }
 
-        function onWindowResize() {
-
+        function onWindowResize() { //make canvas responsive
             camera.aspect = window.innerWidth / window.innerHeight;
             aspect = camera.aspect;
             camera.updateProjectionMatrix();
-
             renderer.setSize( window.innerWidth, window.innerHeight );
-
             render();
 
         }
 
-        function render() {
+        function render() {// refresh view/re-render canvas
+
             scrollDelta = currentScroll - scrollBuffer;
             if(browserName === "Chrome") {scrollDelta *= 0.03;}
-                requestAnimationFrame(render);
-                var delta = clock.getDelta();
-                if (mixer != null) {
-                    mixer.update(delta*scrollDelta*300);
-                    scrollBuffer = currentScroll;
-                    elapsedTime = elapsedTime + delta*scrollDelta*300;
-                    console.log(elapsedTime);
+            requestAnimationFrame(render);
+            var delta = clock.getDelta();
 
-                    console.log(mixer.time)
-                };
+            let updateBuffer = delta*scrollDelta*20*scrollSpeed;
 
-                renderer.render(scene, camera);
-                // console.log(`Height: ${window.innerHeight}, scroll: ${currentScroll}, aspect: ${aspect}`);
-                // if(mainScene)console.log(mainScene.animations);
+            if (mixer != null) {
+
+                //restrict animation to 0.11s - 22.9s (24FPS)
+                if (currentTime > 22.9){currentTime = 22.9;}
+                if (currentTime < 0.11){currentTime = 0.11;}
+
+                currentTime = currentTime + updateBuffer;
+                if(currentTime >= 0.11 && currentTime < 22.9){
+                    //do something if animation is within restricted range
+                currentTime
+                } else {
+                    //don't play animation
+                    updateBuffer = 0;
+                }
+
+                //sync animation time with scroll position
+                mixer.update(updateBuffer);
+                scrollBuffer = currentScroll;
+
+                currentAnimationFrame = mixer.time * 24;
             
+                //hide or unhide clickable divs based on current frame
+                if (currentAnimationFrame > 58 && firstHiddenDiv){
+                    firstHiddenDiv.classList.add('active-div');
+                }
+
+                if (currentAnimationFrame > 75 || currentAnimationFrame < 58){
+                    firstHiddenDiv.classList.remove('active-div');
+                }
+            
+
+                if (currentAnimationFrame > 268 && secondHiddenDiv){
+                    secondHiddenDiv.classList.add('active-div');
+                }
+
+                if (currentAnimationFrame > 284 || currentAnimationFrame < 268){
+                    secondHiddenDiv.classList.remove('active-div');
+                }
+            
+
+                if (currentAnimationFrame > 480 && thirdHiddenDiv){
+                    thirdHiddenDiv.classList.add('active-div');
+                }
+
+                if (currentAnimationFrame > 494 || currentAnimationFrame < 480){
+                    thirdHiddenDiv.classList.remove('active-div');
+                }
+
+            };
 
             renderer.render( scene, camera );
 
         }
 
+//event functions
 function zoom(event) {
-    // event.preventDefault();
     currentScroll += event.deltaY * -0.01;
-  
-    // Restrict scale
-    
-    if(browserName === "Chrome") {
-        currentScroll = Math.min(Math.max(3.8, currentScroll), 170 );
-    } else {
-        currentScroll = Math.min(Math.max(.12, currentScroll), 5.1);
-    }
   }
   
+function firstButtonClick(){
+    window.open('https://www.google.com/');
+}
+function secondButtonClick(){
+    window.open('https://www.amazon.com/');
+}
+function thirdButtonClick(){
+    window.open('https://www.duckduckgo.com/');
+}
+
+  //event listeners
   el.onwheel = zoom;
+  firstHiddenButton.addEventListener('click', firstButtonClick);
+  secondHiddenButton.addEventListener('click', secondButtonClick);
+  thirdHiddenButton.addEventListener('click', thirdButtonClick);
