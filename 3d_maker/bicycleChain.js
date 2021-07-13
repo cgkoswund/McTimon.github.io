cd2a9e85f2f6faf01bb51ff15056ae0f9a06a8ee
McTimon <tnyaffram@gmail.com>
2021-07-13 15:57:54 +0000

    zoomed closer for clearer animation

--- 3d_maker/bicycleChain.js
+++ 3d_maker/bicycleChain.js
@@ -18,8 +18,8 @@ renderer.shadowMap.enabled = true;
 
 camera = new THREE.PerspectiveCamera( 40, (0.78* window.innerWidth) / window.innerHeight, 0.1, 1000 );
 camera.position.set( - 1.1, 1.9, 20.5 );
-camera.position.set(-6, 6, 6).multiplyScalar(3);
-camera.lookAt(2,5,2);
+camera.position.set(-1, 2, 1.5).multiplyScalar(4);
+camera.lookAt(1.8,3.5,2);
 
 
 const scene = new THREE.Scene();
