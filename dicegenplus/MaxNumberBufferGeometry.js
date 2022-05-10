70b6c86e02259369f7da399a4e0b867976cc3929
McTimon <tnyaffram@gmail.com>
2022-05-10 14:24:20 +0000

    Update MaxNumberBufferGeometry.js

--- dicegenplus/MaxNumberBufferGeometry.js
+++ dicegenplus/MaxNumberBufferGeometry.js
@@ -165,7 +165,7 @@ let MaxNumberBufferGeometry = {
   // }
 };
 //Todo some extra commits today
-//done....nnnggccvvvvb
+//done....nnnggccvvvvbgg
 //maybe it'll happen tomorrow
 //or the day after
 
