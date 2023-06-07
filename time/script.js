9d3f6ecd099a5a8203a4210d8d03697cc03f2345
cgkoswund <tnyaffram@gmail.com>
2023-06-07 06:00:54 +0000

    corrected remaining time

--- time/script.js
+++ time/script.js
@@ -1,9 +1,5 @@
 console.log("js is linked");
 const date = new Date();
-console.log("the day is: ", date.getDay()); //day of the week
-console.log("the hours are: ", date.getHours()); //hours elapsed today
-console.log("the mins are: ", date.getMinutes()); //hours left today
-console.log("the mins are: ", date.getSeconds()); //hours left today
 
 //current Time
 const daysPassed = date.getDay();
@@ -35,14 +31,15 @@ const remainingHours = totalHours - hoursPassed;
 const remainingMinutes = totalMinutes - minutesPassed;
 const remainingSeconds = totalSeconds - secondsPassed;
 const remainingAbsoluteHours =
-  remainingDays * 24 - holeHoursPerDay + remainingHours + remainingMinutes / 60;
+  remainingDays * 24 -
+  holeHoursPerDay * remainingDays +
+  remainingHours +
+  remainingMinutes / 60;
 const remainingAbsHrsTrimmed = Math.floor(remainingAbsoluteHours * 1000) / 1000;
 
 //publish
 // const finalInfo = `${remainingDays}:${remainingHours}:${remainingDays} of free time left this week`;
 const finalInfo = `${remainingAbsHrsTrimmed} hrs of free time left this week`;
-console.log("final final:", finalInfo);
-console.log("final converted:", remainingAbsHrsTrimmed);
 
 const textElement = document.querySelector("#timeTextElement");
 textElement.innerHTML = finalInfo;
